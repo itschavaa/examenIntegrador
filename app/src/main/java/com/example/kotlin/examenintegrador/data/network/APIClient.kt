a9f695ae37e61a5157dd53cbb83d70ea95c003fe
Chava Rodríguez <94005146+itschavaa@users.noreply.github.com>
2023-11-26 10:07:51 -0600

    feat: fetch api

---
+++ app/src/main/java/com/example/kotlin/examenintegrador/data/network/APIClient.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin.examenintegrador.data.network
+
+import com.example.kotlin.examenintegrador.data.model.CasesData
+import com.example.kotlin.examenintegrador.utils.Constants
+import java.lang.Exception
+
+class APIClient {
+    private val api: APIService = NetworkModuleDI()
+
+    suspend fun getCases(country: String): CasesData? {
+        return try{
+            api.getCases(key = Constants.KEY, country = country)
+        } catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
+}
