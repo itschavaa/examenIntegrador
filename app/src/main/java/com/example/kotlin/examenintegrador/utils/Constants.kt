85866576eb2dfc77454b83f5ef7517555490ce3f
Chava Rodríguez <94005146+itschavaa@users.noreply.github.com>
2023-11-26 09:24:25 -0600

    feat: object Constants added

---
+++ app/src/main/java/com/example/kotlin/examenintegrador/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin.examenintegrador.utils
+
+object Constants {
+    const val BASE_URL = "https://api.api-ninjas.com/v1/"
+    const val KEY = "HG+tNRBK5wsi+/AfaN9KCw==9VEpNEQuiAAzZL6l"
+}
