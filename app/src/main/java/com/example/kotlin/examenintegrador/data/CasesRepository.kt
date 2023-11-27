1797a4797bccbe01ee05a90dff13521b803c00b9
Chava Rodríguez <94005146+itschavaa@users.noreply.github.com>
2023-11-26 19:51:06 -0600

    fix: argument added on CasesRepository.kt

--- app/src/main/java/com/example/kotlin/examenintegrador/data/CasesRepository.kt
+++ app/src/main/java/com/example/kotlin/examenintegrador/data/CasesRepository.kt
@@ -1,10 +1,9 @@
 package com.example.kotlin.examenintegrador.data
 
-import com.example.kotlin.examenintegrador.data.model.CaseDetails
 import com.example.kotlin.examenintegrador.data.model.CasesData
 import com.example.kotlin.examenintegrador.data.network.APIClient
 
-class CasesRepository {
+class CasesRepository(apiClient: APIClient) {
     private val api = APIClient()
     suspend fun getCases(country: String): List<CasesData>?  = api.getCases(country)
 }
